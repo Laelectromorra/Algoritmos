0ec218387902023bef77765441797a66b8bf3f79
Laelectromorra <149855656+Laelectromorra@users.noreply.github.com>
2023-12-06 05:13:09 -0600

    Cálculo del área de un triángulo

--- Main.java
+++ Main.java
@@ -1,11 +1,9 @@
 import javax.swing.JOptionPane;
 public class Main {
     public static void main(String[] args) {
-        String name=JOptionPane.showInputDialog("Ingresa tu nombre");
-        JOptionPane.showMessageDialog(null,"Hola, "+name);
-        int edad= Integer.parseInt(JOptionPane.showInputDialog("Ingresa tu edad"));
-        JOptionPane.showMessageDialog(null,"Hola, tú tienes "+edad+ " años");
-        double estatura= Double.parseDouble(JOptionPane.showInputDialog("Ingresa tu estatura"));
-        JOptionPane.showMessageDialog(null,"Hola, tú mides "+estatura+ " metros");
+        double base= Double.parseDouble(JOptionPane.showInputDialog("Ingresa la base del triángulo:"));
+        double altura= Double.parseDouble(JOptionPane.showInputDialog("Ingresa la altura del triángulo:"));
+        double area= (base*altura)/2;
+        JOptionPane.showMessageDialog(null,"El área del triángulo es igual a "+area+ " cm2");
     }
 }
